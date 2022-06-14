942cd7ca0c75095c9e1cdc9af4095104600efdd7
Ocawesome101 <ocawesome101@gmail.com>
2022-06-14 18:58:46 -0400

    `ps` now accepts `a` as an argument to print a little more info

--- src/ps.lua
+++ src/ps.lua
@@ -1,6 +1,7 @@
 --!lua
 -- ps implementation
--- takes no arguments at this time
+
+local argv = ...
 
 local dirent = require("posix.dirent")
 
@@ -12,7 +13,7 @@ for i=#files, 1, -1 do
   end
 end
 
-table.sort(files)
+table.sort(files, function(a,b) return tonumber(a) < tonumber(b) end)
 
 local function read(f)
   local h = io.open(f, "r")
@@ -22,9 +23,20 @@ end
 print("  PID  PPID   UID CMD")
 for i=1, #files, 1 do
   local path = "/proc/"..files[i]
+  local cmdline = {}
+  if argv[1] == "a" then
+    local cmdlinefiles = dirent.dir(path.."/cmdline/")
+    table.sort(cmdlinefiles, function(a,b) return tonumber(a) < tonumber(b) end)
+    for _, file in ipairs(cmdlinefiles) do
+      cmdline[#cmdline + 1] = read(path.."/cmdline/"..file)
+    end
+    cmdline = table.concat(cmdline, " ")
+  else
+    cmdline = read(path.."/cmdline/0")
+  end
   print(string.format("%5s %5s %5s %s",
     read(path.."/pid"),
     read(path.."/ppid"),
     read(path.."/uid"),
-    read(path.."/cmdline/0")))
+    cmdline))
 end
